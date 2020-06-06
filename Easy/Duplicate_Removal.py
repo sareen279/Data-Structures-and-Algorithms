95021cb1695a9987deab34c1877ad5fb7c80031c
Sareen Shah <sareen279@gmail.com>
2020-06-06 21:08:00 +0530

    Made a change to 'Duplicate elements removal from Array' code in Easy Folder

--- Easy/Duplicate_Removal.py
+++ Easy/Duplicate_Removal.py
@@ -1,6 +1,16 @@
 #Remove duplicate elements from sorted Array
 #Given a sorted array A of size N. The function remove_duplicate takes two arguments . The first argument is the sorted array A[ ] and the second argument is 'N' the size of the array and returns the size of the new converted array A[ ] with no duplicate element.
 #n -> length of array, arr -> array
+#Example:
+#Input:
+#2
+#5
+#2 2 2 2 2
+#3
+#1 2 2
+#Output:
+#2
+#1 2
 def remove_duplicate(n, arr):
     i = 0
     while(i < len(arr)-1):
